0261d654f34111d5d861ca88cb13d6698d1afae0
Mallipudijaya <42345244+Mallipudijaya@users.noreply.github.com>
2022-09-10 19:55:44 -0500

    SEHW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,36 +1,68 @@
 # frozen_string_literal: true
 
 # Part 1
-
 def sum(arr)
-  # YOUR CODE HERE
+  sum=0    
+  arr.each{|i| sum+=i}
+  return sum
 end
 
+
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  l=arr.length
+  if(l==0)
+    res= 0
+  elsif(l==1)
+    res= arr[0]
+  else
+    arr=arr.sort
+    res= arr[l-1]+arr[l-2]
+  return res
+  end
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  return false if arr.empty? 
+  arr.combination(2).any? {|i, j| i + j == number }
 end
 
-# Part 2
 
+# Part 2
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+(name)
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  if /^[^aeiou\W]/i.match(string) == nil
+    return false
+  else
+    return true
+  end  
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  return true if string == "0"
+	if /^[10]*00$/.match(string) != nil
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
-
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  attr_accessor :isbn
+	attr_accessor :price
+
+	def initialize(isbn, price)
+		raise ArgumentError, 
+			"Illegal argument" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		sprintf("$%2.2f", @price)
+	end
 end
